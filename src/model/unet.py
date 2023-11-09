a2e7de9b2fd489d26fc22a2c7330431ade7ca17d
Võ Đình Đạt <vodinhdat03@gmail.com>
2023-11-09 14:15:29 +0700

    add unet

---
+++ src/model/unet.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+def _make_layers(in_channels: int, out_channels: int):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.down = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            _make_layers(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.down(x)
+
+
+class UpSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.convT = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = _make_layers(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.convT(x1)
+        out = torch.cat([x1, x2], dim=1)
+        out = self.conv(out)
+        return out
+
+
+class UNet(nn.Module):
+    def __init__(
+        self,
+        in_channels: int
+    ):
+        super().__init__()
+        self.conv_in = _make_layers(in_channels, 64)
+        self.down1 = DownSample(64, 128)
+        self.down2 = DownSample(128, 256)
+        self.down3 = DownSample(256, 512)
+        self.down4 = DownSample(512, 1024)
+        self.up1 = UpSample(1024, 512)
+        self.up2 = UpSample(512, 256)
+        self.up3 = UpSample(256, 128)
+        self.up4 = UpSample(128, 64)
+        self.conv_out = _make_layers(64, in_channels)
+
+    def forward(self, x):
+        x1 = self.conv_in(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x = self.down4(x4)
+        x = self.up1(x, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.conv_out(x)
+        return x
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = UNet(in_channels=3).to(device)
+    print(summary(model, input_size=(3, 256, 256), device=device.type))
