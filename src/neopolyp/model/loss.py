1b2ca539d0cacc4c09421ec315a8899ccafb7869
Võ Đình Đạt <vodinhdat03@gmail.com>
2023-11-10 09:28:28 +0700

    add focal tversky loss

--- src/neopolyp/model/loss.py
+++ src/neopolyp/model/loss.py
@@ -2,19 +2,58 @@ import torch
 from torch import Tensor
 
 
-def dice_loss(logits: Tensor, target: Tensor, epsilon: float = 1e-6):
+def dice_score(
+    logits: Tensor,
+    target: Tensor,
+    smooth: float = 1e-6
+):
     # Average of Dice coefficient for all batches, or for a single mask
-    sum_dim = (-1, -2, -3)
-    out_softmax = torch.softmax(logits, dim=1)
-    inter = 2 * (out_softmax * target).sum(dim=sum_dim)
-    sets_sum = out_softmax.sum(dim=sum_dim) + target.sum(dim=sum_dim)
-    sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
+    inputs = torch.softmax(logits, dim=1)
 
-    dice = (inter + epsilon) / (sets_sum + epsilon)
-    return 1 - dice.mean()
+    # flatten label and prediction tensors
+    inputs = inputs.reshape(-1)
+    targets = target.reshape(-1)
+
+    intersection = (inputs * targets).sum()
+    dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)
+
+    return 1 - dice
+
+
+def dice_loss(
+    logits: Tensor,
+    target: Tensor,
+    epsilon: float = 1e-6
+):
+    return 1 - dice_score(logits, target, epsilon)
+
+
+def focal_tversky_loss(
+    logits: Tensor,
+    targets: Tensor,
+    smooth: float = 1e-6,
+    alpha: float = 0.5,
+    beta: float = 0.5,
+    gamma: float = 1
+):
+
+    # flatten label and prediction tensors
+    logits = logits.reshape(-1)
+    targets = targets.reshape(-1)
+
+    # True Positives, False Positives & False Negatives
+    TP = (logits * targets).sum()
+    FP = ((1-targets) * logits).sum()
+    FN = (targets * (1-logits)).sum()
+
+    tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)
+    focal_tversky = (1 - tversky)**gamma
+
+    return focal_tversky
 
 
 if __name__ == "__main__":
     a = torch.rand(4, 3, 8, 8)
     b = torch.randint(0, 2, (4, 3, 8, 8))
     print(dice_loss(a, b))
+    print(focal_tversky_loss(a, b))
--- src/neopolyp/model/model.py
+++ src/neopolyp/model/model.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from .unet import UNet
-from .loss import dice_loss
+from .loss import dice_score, focal_tversky_loss
+
 
 class NeoPolypModel(pl.LightningModule):
     def __init__(self, lr: float = 1e-4):
@@ -17,20 +18,26 @@ class NeoPolypModel(pl.LightningModule):
         image, mask = batch['image'].float(), batch['mask'].permute(0, 3, 1, 2)
         y_hat = self(image)
         e_loss = F.cross_entropy(y_hat, torch.argmax(mask, dim=1))
-        d_loss = dice_loss(y_hat, mask)
-        loss = e_loss + d_loss
-        self.log("train_loss", loss)
-        self.log("train_entropy_loss", e_loss)
-        self.log("train_dice_loss", d_loss)
+        f_loss = focal_tversky_loss(y_hat, mask)
+        d_score = dice_score(y_hat, mask)
+        loss = e_loss + f_loss
+        self.log("train_loss", loss, on_epoch=True)
+        self.log("train_entropy_loss", e_loss, on_epoch=True)
+        self.log("train_ft_loss", f_loss, on_epoch=True)
+        self.log("train_dice_score", d_score, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         image, mask = batch['image'].float(), batch['mask'].permute(0, 3, 1, 2)
         y_hat = self(image)
         e_loss = F.cross_entropy(y_hat, torch.argmax(mask, dim=1))
-        d_loss = dice_loss(y_hat, mask)
-        loss = e_loss + d_loss
-        self.log("val_loss", e_loss)
+        f_loss = focal_tversky_loss(y_hat, mask)
+        d_score = dice_score(y_hat, mask)
+        loss = (e_loss + f_loss) / 2
+        self.log("val_loss", loss, on_epoch=True)
+        self.log("val_entropy_loss", e_loss, on_epoch=True)
+        self.log("val_ft_loss", f_loss, on_epoch=True)
+        self.log("val_dice_score", d_score, on_epoch=True)
         return loss
 
     def configure_optimizers(self):
