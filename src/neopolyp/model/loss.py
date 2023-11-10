59b0eed1c5dd43e901951abb08f8636820f293c2
Võ Đình Đạt <vodinhdat03@gmail.com>
2023-11-10 09:05:43 +0700

    fix focal loss

--- src/neopolyp/model/loss.py
+++ src/neopolyp/model/loss.py
@@ -2,20 +2,19 @@ import torch
 from torch import Tensor
 
 
-def dice_loss(
-    image: Tensor,
-    mask: Tensor,
-    epsilon: float = 1e-6
-):
-    inter = 2 * (image * mask).sum()
-    sets_sum = image.sum() + mask.sum()
+def dice_loss(logits: Tensor, target: Tensor, epsilon: float = 1e-6):
+    # Average of Dice coefficient for all batches, or for a single mask
+    sum_dim = (-1, -2, -3)
+    out_softmax = torch.softmax(logits, dim=1)
+    inter = 2 * (out_softmax * target).sum(dim=sum_dim)
+    sets_sum = out_softmax.sum(dim=sum_dim) + target.sum(dim=sum_dim)
     sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
 
     dice = (inter + epsilon) / (sets_sum + epsilon)
-    return dice
+    return 1 - dice.mean()
 
 
 if __name__ == "__main__":
-    image = torch.rand(4, 3, 256, 256)
-    mask = torch.rand(4, 3, 256, 256)
-    print(dice_loss(image.flatten(0, 1), mask.flatten(0, 1)))
+    a = torch.rand(4, 3, 8, 8)
+    b = torch.randint(0, 2, (4, 3, 8, 8))
+    print(dice_loss(a, b))
--- src/neopolyp/model/model.py
+++ src/neopolyp/model/model.py
@@ -4,7 +4,6 @@ import pytorch_lightning as pl
 from .unet import UNet
 from .loss import dice_loss
 
-
 class NeoPolypModel(pl.LightningModule):
     def __init__(self, lr: float = 1e-4):
         super().__init__()
@@ -17,24 +16,20 @@ class NeoPolypModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         image, mask = batch['image'].float(), batch['mask'].permute(0, 3, 1, 2)
         y_hat = self(image)
-        e_loss = F.cross_entropy(y_hat, mask.float())
-        # d_loss = dice_loss(
-        #     F.softmax(y_hat, dim=1).float().flatten(0, 1),
-        #     F.one_hot(mask.long(), 3).permute(0, 3, 1, 2).float().flatten(0, 1)
-        # )
-        loss = e_loss  # + d_loss
-        self.log("train_loss", e_loss)
+        e_loss = F.cross_entropy(y_hat, torch.argmax(mask, dim=1))
+        d_loss = dice_loss(y_hat, mask)
+        loss = e_loss + d_loss
+        self.log("train_loss", loss)
+        self.log("train_entropy_loss", e_loss)
+        self.log("train_dice_loss", d_loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         image, mask = batch['image'].float(), batch['mask'].permute(0, 3, 1, 2)
         y_hat = self(image)
-        e_loss = F.cross_entropy(y_hat, mask.float())
-        # d_loss = dice_loss(
-        #     F.softmax(y_hat, dim=1).float().flatten(0, 1),
-        #     F.one_hot(mask.long(), 3).float().flatten(0, 1)
-        # )
-        loss = e_loss  # + d_loss
+        e_loss = F.cross_entropy(y_hat, torch.argmax(mask, dim=1))
+        d_loss = dice_loss(y_hat, mask)
+        loss = e_loss + d_loss
         self.log("val_loss", e_loss)
         return loss
 
