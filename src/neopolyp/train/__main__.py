9fe90577e82210591f6a32b146119874c2360077
Võ Đình Đạt <vodinhdat03@gmail.com>
2024-01-16 09:35:22 +0700

    update name

--- src/neopolyp/train/__main__.py
+++ src/neopolyp/train/__main__.py
@@ -59,7 +59,7 @@ def main():
     # WANDB (OPTIONAL)
     if args.wandb:
         wandb.login(key=args.wandb_key)  # API KEY
-        name = f"unet-{args.max_epochs}-{args.batch_size}-{args.lr}"
+        name = f"{args.model}-{args.max_epochs}-{args.batch_size}-{args.lr}"
         logger = WandbLogger(project="deep-learning-exercise",
                              name=name,
                              log_model="all")
