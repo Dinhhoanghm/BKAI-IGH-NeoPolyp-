9718882ed4d06e97f2a5dccddc7b345f64fa35b1
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2024-11-22 22:08:46 +0700

    first commit

--- script/setup.sh
+++ script/setup.sh
@@ -6,7 +6,7 @@ if [ -d "bkai-igh-neopolyp" ]; then
   echo "bkai-igh-neopolyp already downloaded"
 else
   echo "bkai-igh-neopolyp not downloaded, downloading now"
-  curl -L "https://www.kaggle.com/competitions/30892/download-all" > "bkai-igh-neopolyp.zip"
+  curl -L "https://drive.usercontent.google.com/download?id=1UnWSmQZFiLLhscTEiCGIpsLdBFNXcTlt&export=download&authuser=0&confirm=t&uuid=862dfef3-962b-45da-873e-d1a052f691c6&at=AENtkXY8TXAmYlgKKm6iiT7hu2zD:1732288083551" > "bkai-igh-neopolyp.zip"
   unzip -q "bkai-igh-neopolyp.zip" -d "bkai-igh-neopolyp"
 fi
 
