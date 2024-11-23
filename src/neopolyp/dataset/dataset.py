9c60e01b3cd1d6587206fc1d1fa9272e71216fee
Dinhhhoanghm <dodinhhoanghm@gmail.com>
2024-11-23 09:11:35 +0700

    first commit

--- src/neopolyp/dataset/dataset.py
+++ src/neopolyp/dataset/dataset.py
@@ -2,14 +2,13 @@ import neopolyp
 from torch.utils.data import Dataset
 import cv2
 import numpy as np
-from typing import Union
 
 
 class NeoPolypDataset(Dataset):
     def __init__(
         self,
         image_dir: list,
-        gt_dir: Union[list, None] = None,,
+        gt_dir: list | None = None,
         session: str = "train",
         transform: bool = True,
     ) -> None:
